82ca59e87842cd05c54d1df23e2c683a1d921085
wencui <wencui2008@gmail.com>
2019-07-27 19:36:21 -0400

    Master commit

--- NutritionalMeal/src/Test.java
+++ NutritionalMeal/src/Test.java
@@ -4,11 +4,10 @@ public class Test {
 	// test for reverse the class
 	public static void main(String[] args) {
 		// master
+		int master = 1;
 	}
 	
 	// brannch function
 	public static void thisIsBranchFunction() {
-		// 
-		int i = 10;
 	}
 }
